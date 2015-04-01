e61dd371e66043b7a2557f2a49ec92d9df8c5e36
Andrew Fan <andrewsuicune@gmail.com>
2015-04-01 15:51:54 -0400

    More Reimu

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 ## Random Spellcard Name Generator ##
 This is a little project of mine that will generate spellcard names. Although it's mainly for fun, I may expand it/contort it.
 
+Note that the 'sign' portion of the spell name is random - 'Dream Sign' has the same likelihood of appearing that 'Light Spirit' has.
+
 To compile:
+
 make
 
 To run:
+
 ./generator
+
+Supported Characters:
+
+-Reimu Hakurei "Reimu"
--- generator_fxn.cc
+++ generator_fxn.cc
@@ -14,22 +14,50 @@ using std::cin;
 using std::endl;
 using std::string;
 
-/*Generates: "<sign>: <noun> of <noun2>" or "<sign> Sign: <adj> <noun>"                 
+/*Generates: "<sign>: <noun> of <action>" or "<sign> Sign: <adj> <noun>"
 */
 
 Generator::Generator(string character){
   //if character is DEFAULT, will use all available signs, adjectives, and nouns 
-  if(character=="Reimu" || character=="DEFAULT"){//Covering:
+  if(character=="Reimu" || character=="DEFAULT"){
+    //Touhou Canon
     sign.push_back("Spirit Sign");
     sign.push_back("Dream Sign");
     sign.push_back("Divine Spirit");
+    sign.push_back("Divine Arts");
+    sign.push_back("Scattered Spirit");
+    sign.push_back("Migrating Spirit");
+    sign.push_back("Dream Land");
+    sign.push_back("Treasure Sign");
+    sign.push_back("Holy Relic");
+    sign.push_back("Power Sign");
+    sign.push_back("Dream Battle");
+    sign.push_back("Jewel Sign");
+    sign.push_back("Prayer");
+    sign.push_back("Light Spirit");
+    sign.push_back("Barrier");
+    sign.push_back("Treasure");
+    sign.push_back("Charm");
+    sign.push_back("Talisman");
+    sign.push_back("Amulet");
+    sign.push_back("Omikuji");
     adj.push_back("Fantasy");
     adj.push_back("Omnidirectional");
     adj.push_back("Demon-Binding");
     adj.push_back("Dragon-Slaying");
+    adj.push_back("Duplex");
+    adj.push_back("Hakurei");
+    adj.push_back("Yin-Yang");
     noun.push_back("Seal");
     noun.push_back("Circle");
-    noun2.push_back("Exorcism");
+    noun.push_back("Orb");
+    noun.push_back("Treasured Orb");
+    noun.push_back("Moon");
+    noun.push_back("Orb String");
+    noun.push_back("Demon God Orb");
+    noun.push_back("Barrier");
+    noun.push_back("Illusion");
+    action.push_back("Exorcism");
   }
   
 }
@@ -38,7 +66,7 @@ Generator::Generator(Generator& old){
   sign = old.sign;
   adj = old.adj;
   noun = old.noun;
-  noun2 = old.noun2;
+  action = old.action;
 }
 
 Generator::~Generator(){
@@ -51,7 +79,7 @@ void Generator::GenerateA1(){
 }
 
 void Generator::GenerateA2(){
-  string toprint = sign.at(rand()%sign.size()) + ": "+ noun.at(rand()%noun.size()) + " of " + noun2.at(rand()%noun2.size());
+  string toprint = sign.at(rand()%sign.size()) + ": "+ noun.at(rand()%noun.size()) + " of " + action.at(rand()%action.size());
   cout << toprint << endl;
 }
 
